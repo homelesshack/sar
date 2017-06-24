7166d3d259ab3dbc2c66ca4dcaef74bf5d27fea0
Chris Blackburn <christopher.blackburn@gmail.com>
2017-06-24 13:30:46 +0100

    Dynamic json values

--- app/app/controllers/price_discrepancy_reports_controller.rb
+++ app/app/controllers/price_discrepancy_reports_controller.rb
@@ -3,23 +3,44 @@ class PriceDiscrepancyReportsController < ApplicationController
   end
 
   def show
-    brma = SAR::BRMA::FindByPostcode.new(params[:postcode]).convert
+
+p spareroom
 
     render json: {
       brma: brma[:name],
-      government_allowance: 590,
+      government_allowance: housing_allowance[:lha_rate],
       results: {
         spareroom: {
-          total_results: 1099,
-          highest_rent: 830,
-          lowest_rent: 80,
-          mean_rent: 394,
-          median_rent: 215,
-          rooms_below_threshold: 5
+          total_results: spareroom[:total_results],
+          highest_rent: spareroom[:highest_rent],
+          lowest_rent: spareroom[:lowest_rent],
+          mean_rent: spareroom[:mean_rent],
+          median_rent: spareroom[:median_rent],
+          rooms_below_threshold: rooms_below_threshold
         }
       },
-      number_rooms: 1099,
-      rooms_below_threshhold: 5
-  }
+      number_rooms: spareroom[:total_results],
+      rooms_below_threshhold: rooms_below_threshold
+    }
+  end
+
+  private
+
+  def brma
+    @brma ||= SAR::BRMA::FindByPostcode.new(params[:postcode]).convert
+  end
+
+  def housing_allowance
+    @housing_allowance ||= SAR::BRMA::HousingAllowanceFinder.new.exec(brma[:brma])
+  end
+
+  def spareroom
+    @spareroom ||= SAR::Spareroom::SearchScraper.new.run(params[:postcode])
+  end
+
+  def rooms_below_threshold
+    @rooms_below_threshold ||= spareroom[:data].select do |price|
+      price <= housing_allowance[:lha_rate]
+    end.count
   end
 end
