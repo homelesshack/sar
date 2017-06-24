3c496b79de96de1f8dad961f5b89b273d4badfd4
Chris Blackburn <christopher.blackburn@gmail.com>
2017-06-24 12:54:10 +0100

    Initial implementation of live JSON report

--- app/app/controllers/price_discrepancy_reports_controller.rb
+++ app/app/controllers/price_discrepancy_reports_controller.rb
@@ -2,6 +2,24 @@ class PriceDiscrepancyReportsController < ApplicationController
   def index
   end
 
-  def create
+  def show
+    brma = SAR::BRMA::FindByPostcode.new(params[:postcode]).convert
+
+    render json: {
+      brma: brma[:name],
+      government_allowance: 590,
+      results: {
+        spareroom: {
+          total_results: 1099,
+          highest_rent: 830,
+          lowest_rent: 80,
+          mean_rent: 394,
+          median_rent: 215,
+          rooms_below_threshold: 5
+        }
+      },
+      number_rooms: 1099,
+      rooms_below_threshhold: 5
+  }
   end
 end
