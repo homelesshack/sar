73935b746fbaac94cf61e23dcdd9775ed57fa7a7
Emile <emile.swarts123@gmail.com>
2017-06-24 12:22:20 +0100

    Use csv headers

--- app/lib/sar/brma/housing_allowance_finder.rb
+++ app/lib/sar/brma/housing_allowance_finder.rb
@@ -7,25 +7,21 @@ module SAR
         result = find_by(brma_number)
 
         {
-          bmra_name: result[NAME_INDEX],
-          lha_rate: result[LHA_RATE_INDEX].to_f
+          bmra_name: result['BRMAname'],
+          lha_rate: result['LHArate'].to_f
         }
       end
 
       private
 
-      NAME_INDEX = 2
-      LHA_RATE_INDEX = 6
-
       def find_by(brma_number)
-        @result_row ||= parsed_csv.select do |row|
-          row_number, _ = row
-          row_number.to_i == brma_number
-        end.flatten
+        @result_row ||= parsed_csv.find do |row|
+          row['BRMAno'].to_i == brma_number
+        end
       end
 
       def parsed_csv
-        CSV.read(Rails.root.join('fixtures', 'brma.csv'))
+        CSV.read(Rails.root.join('fixtures', 'brma.csv'), headers: true)
       end
     end
   end
